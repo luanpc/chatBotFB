cf9a69eb8f06a1ff25bd6af12325fc6cfe1a704a
luanpc <luanpc2805@gmail.com>
2022-02-12 22:35:34 +0700

    intializing project setting

---
+++ src/routes/web.js
@@ -0,0 +1,10 @@
+import express from "express";
+import homeController from '../controllers/HomeController';
+
+let router = express.Router();
+let initWebRoutes = (app) => {
+    router.get("/", homeController.getHomePage);
+
+    return app.use('/', router);
+}
+module.exports = initWebRoutes;
