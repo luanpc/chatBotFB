293011e50aaa875ad9eb22b068af824564edf986
luanpc <luanpc2805@gmail.com>
2022-02-13 15:21:43 +0700

    update

--- src/routes/web.js
+++ src/routes/web.js
@@ -5,6 +5,8 @@ let router = express.Router();
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
 
+    router.post("/webhook", homeController.postWebhook);
+    router.get("/webhook", homeController.getWebhook);
     return app.use('/', router);
 }
 module.exports = initWebRoutes;
